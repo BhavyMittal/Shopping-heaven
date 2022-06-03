fd012b31fddd39832fa13757c970e8d288f4ec72
BhavyMittal <98758245+BhavyMittal@users.noreply.github.com>
2022-06-03 17:27:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function reg()
+{
+    user_input = document.getElementById("uname").value;
+    localStorage.setItem("user",user_input);
+}
+
---
+++ buy.js
@@ -0,0 +1,3 @@
+user_ka_name = localStorage.getItem("user");
+
+document.getElementById("name").innerHTML ="You have Been registered with your account name -" + user_ka_name;
